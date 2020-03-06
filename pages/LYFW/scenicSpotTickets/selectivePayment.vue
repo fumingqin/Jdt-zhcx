add924daa8617b90e3877eda77aa70fee37e85b5
傅洺钦 <32589407@qq.com>
2020-03-06 21:29:59 +0800

    1、修改了门票列表、门票详情、填写订单的交互流程 2、完善了优惠券、人数、价格之间的算法关联 3、添加了旅游服务主页，做跳转使用

--- pages/LYFW/scenicSpotTickets/selectivePayment.vue
+++ pages/LYFW/scenicSpotTickets/selectivePayment.vue
@@ -8,9 +8,18 @@
 	export default {
 		data() {
 			return {
-				
+				 
 			}
 		},
+		onLoad : function(options) {
+			console.log(JSON.parse(decodeURIComponent(options.ticket)))
+			console.log(JSON.parse(decodeURIComponent(options.addressData)))
+			console.log(JSON.parse(options.actualPayment))
+			console.log(JSON.parse(options.dateReminder))
+			console.log(JSON.parse(options.date))
+			console.log(JSON.parse(decodeURIComponent(options.coupon)))
+		},
+		
 		methods: {
 			
 		}
