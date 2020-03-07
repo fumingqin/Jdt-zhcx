e8913ce37527d0521dbba072f3fef4da213c0927
Gongjs <1845553629@qq.com>
2020-03-07 11:33:21 +0800

    1.添加新增地址页面

--- pages/GRZX/address.vue
+++ pages/GRZX/address.vue
@@ -11,7 +11,7 @@
 					<text class="mobile">{{item.mobile}}</text>
 				</view>
 			</view>
-			<text class="yticon icon-bianji" @click.stop="addAddress('edit', item)"></text>
+			<text class="jdticon icon-bianji" @click.stop="addAddress('edit', item)"></text>
 		</view>
 		<text style="display:block;padding: 16upx 30upx 10upx;lihe-height: 1.6;color: #fa436a;font-size: 24upx;">
 			重要：添加和修改地址回调仅增加了一条数据做演示，实际开发中将回调改为请求后端接口刷新一下列表即可
@@ -60,7 +60,8 @@
 			},
 			addAddress(type, item){
 				uni.navigateTo({
-					url: `/pages/GRZX/addressManage?type=${type}&data=${JSON.stringify(item)}`
+					/* url: `/pages/GRZX/addressManage?type=${type}&data=${JSON.stringify(item)}` */
+					url:'/pages/GRZX/addAddress'
 				})
 			},
 			//添加或修改成功之后回调
