78360fdb8d2c2b470e1cbdbd1dc22c4c884ecef5
ChenKz <352646086@qq.com>
2020-03-10 18:41:16 +0800

    传统客运修改  班次列表、主页传统客运部分、填写订单

--- pages/CTKY/selectTickets.vue
+++ pages/CTKY/selectTickets.vue
@@ -8,30 +8,71 @@
 						<view class="dateClass">{{item.date}}</view>
 					</view>
 				</view>
-				<!-- <view style="width: 50%;height: 100%; background: #007AFF; display: inline-block;"></view>
-				<view style="width: 50%;height: 100%; background: #4CD964;display: inline-block;"></view>
-				<view style="width: 50%;height: 100%; background: #999999;display: inline-block;"></view> -->
 			</scroll-view>
 			<view>
-				<image src="../../static/index/calendar.png" class="calendarImage" @click="onShowDatePicker('date')"></image>
+				<image src="../../static/CTKY/calendar.png" class="calendarImage" @click="onShowDatePicker('date')"></image>
 			</view>
 			<mx-date-picker :show="showPicker" :type="type" :value="value" :show-tips="true" :begin-text="'入住'" :end-text="'离店'"
 			 :show-seconds="true" @confirm="onSelected" @cancel="onSelected" />
 		</view>
-		<view class="ticketClass" v-for="(index,i) in 7" :key="index"  @click="ticketDetail">
-			<image src="../../static/index/ordinaryShuttle.png" class="ticketSortImage"></image>
-			<view class="ticketTimeCLass">  
-				<text class="timeClass">15:05</text>
-				<text class="estimatedTimeClass">约1-2小时</text>
+		<view class="ctky_View" v-for="(item,index) in 2" :key="index" @click="ticketDetail">
+			<view class="ctky_View_Left">
+				<view style="display: flex;align-items: center;margin:20upx 25upx;">
+					<view style="width:65upx ;height: 37upx;border-radius: 14upx; border:#1EA2FF  solid 1px;text-align: center;align-items: center;color:#1EA2FF 
+					;font-size: 24upx;font-family: SourceHanSansSC-Light;">传统</view>
+				    <view style="margin-left:19upx ;font-family: SourceHanSansSC-Bold;font-weight: bold;">15:05</view>
+				</view>
+				<!-- <view style="margin:28upx 25upx;font-style: SourceHanSansSC-Regular; font-size:36upx ;color: #2C2D2D;padding: 0;">传统班车</view> -->
+				<view style="margin-left: 25upx;display: flex;align-items: center;margin-bottom: 16upx;">
+					<image src="../../static/CTKY/startDot.png" style="width: 10upx ;height: 10upx;"></image>
+					<view style="margin-left: 16upx; font-size: 30upx;font-style:SourceHanSansSC-Regular ;
+					color: #333333;">泉州客运中心站</view>
+				</view>
+				<view style="margin-left: 25upx;display: flex;align-items: center;margin-bottom: 16upx;">
+					<image src="../../static/CTKY/endDot.png" style="width: 10upx ;height: 10upx;"></image>
+					<view style="margin-left: 16upx;font-size: 30upx;font-style:SourceHanSansSC-Regular ;
+					color: #333333;">安溪</view>
+				</view>
+				<view style="margin-left: 25upx;margin-bottom: 20upx;font-style: SourceHanSansSC-Light;font-weight: lighter;
+				font-size: 28upx;color: #666666;">大型高一/约1-2小时/儿童半票</view>
 			</view>
-			<view class="ticketStationClass">
-				<text class="startClass">泉州客运中心站</text>
-				<text class="endClass">安溪</text>
-				<text class="carTypeClass">大型高一</text>
+			<view class="ctky_View_Right">
+				<view>
+					<view style="margin-right: 28upx;font-size: 36upx;font-style:
+		           SourceHanSansSC-Regular; color: #FC4646;">￥28</view>
+					<view style="margin-right: 28upx;margin-top: 20upx;font-size: 24upx;font-style:
+		           SourceHanSansSC-Light; color: #666666;">余18张</view>
+				</view>
 			</view>
-			<view class="priceBlockClass">
-				<text class="priceClass">￥28</text>
-				<text class="ticketCountClass">10张</text>
+		</view>
+		<view class="ctky_View" v-for="(item,index) in 2" :key="index" @click="ticketDetail">
+			<view class="ctky_View_Left">
+				<view style="display: flex;align-items: center;margin:20upx 25upx;">
+					<view style="width:65upx ;height: 37upx;border-radius: 14upx; border:#FF5A00  solid 1px;text-align: center;align-items: center;color:#FF5A00 
+					;font-size: 24upx;font-family: SourceHanSansSC-Light;">定制</view>
+				    <view style="margin-left:19upx ;font-family: SourceHanSansSC-Bold;font-weight: bold;">15:05</view>
+				</view>
+				<!-- <view style="margin:28upx 25upx;font-style: SourceHanSansSC-Regular; font-size:36upx ;color: #2C2D2D;padding: 0;">传统班车</view> -->
+				<view style="margin-left: 25upx;display: flex;align-items: center;margin-bottom: 16upx;">
+					<image src="../../static/CTKY/startDot.png" style="width: 10upx ;height: 10upx;"></image>
+					<view style="margin-left: 16upx; font-size: 30upx;font-style:SourceHanSansSC-Regular ;
+					color: #333333;">泉州客运中心站</view>
+				</view>
+				<view style="margin-left: 25upx;display: flex;align-items: center;margin-bottom: 16upx;">
+					<image src="../../static/CTKY/endDot.png" style="width: 10upx ;height: 10upx;"></image>
+					<view style="margin-left: 16upx;font-size: 30upx;font-style:SourceHanSansSC-Regular ;
+					color: #333333;">安溪</view>
+				</view>
+				<view style="margin-left: 25upx;margin-bottom: 20upx;font-style: SourceHanSansSC-Light;font-weight: lighter;
+				font-size: 28upx;color: #666666;">大型高一/约1-2小时/儿童半票/站外上车</view>
+			</view>
+			<view class="ctky_View_Right">
+				<view>
+					<view style="margin-right: 28upx;font-size: 36upx;font-style:
+		           SourceHanSansSC-Regular; color: #FC4646;">￥28</view>
+					<view style="margin-right: 28upx;margin-top: 20upx;font-size: 24upx;font-style:
+		           SourceHanSansSC-Light; color: #666666;">余18张</view>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -135,7 +176,7 @@
 
 	.headerClass {
 		width: 100%;
-		background: #F8F8F8;
+		background: #FFFFFF;
 		height: 109upx;
 		padding-left: 10upx;
 		margin-bottom: 10upx;
@@ -192,99 +233,27 @@
 		margin-top: 33upx;
 		margin-bottom: 33upx;
 	}
-	//车票块
-	.ticketClass{
-		width: 100%;
-		height: 176upx;
-		margin-top: 10upx;
-		margin-bottom: 10upx;
+	
+	.ctky_View {
+		width: 706upx;
 		background: #FFFFFF;
+		margin: 16upx 22upx;
+		border-radius: 20upx;
 		display: flex;
+		justify-content: space-between;
 	}
-	//车票类型
-	.ticketSortImage{
-		margin-top: 0;
-		margin-left: 0;
-		width: 95upx;
-		height: 94upx;
-		position: absolute;
-	}
-	//时间块
-	.ticketTimeCLass{
-		text-align: center;
-		width: 269upx;
-	}
-	.timeClass{
-		display: block;
-		margin-top: 27upx;
-		font-size: 34upx;
-		font-family: MicrosoftYaHei;
-		font-weight: 400;
-		color: #333333;
-		text-align: center;
-	}
-	.estimatedTimeClass{
-		display: block;
-		margin-top: 11upx;
-		font-size: 24upx;
-		font-family: MicrosoftYaHei;
-		font-weight: 400;
-		color: #999999;
-		text-align: center;
-	}
-	.ticketStationClass{
-		margin-left: 32upx;
-		height: 100%;
+	
+	.ctky_View_Left {
 		text-align: left;
+		display: flex;
+		flex-direction: column;
+		padding: 0;
 	}
-	.startClass{
-		margin-top: 24upx;
-		display: block;
-		font-size: 30upx;
-		font-family: MicrosoftYaHei;
-		font-weight: 500;
-		color: #333333;
-		//text-align: left;
-	}
-	.endClass{
-		margin-top: 12upx;
-		display: block;
-		font-size: 30upx;
-		font-family: MicrosoftYaHei;
-		font-weight: 500;
-		color: #333333;
-		//text-align: left;
-	}
-	.carTypeClass{
-		display: block;
-		margin-top: 8upx;
-		font-size: 24upx;
-		font-family: MicrosoftYaHei;
-		font-weight: 400;
-		color: #999999;
-		//text-align: left;
-	}
-	.priceBlockClass{
-		width: 122upx;
-		text-align: center;
-		position: absolute;
-		right: 0;
-	}
-	.priceClass{
-		display: block;
-		margin-top: 26upx;
-		font-size: 34upx;
-		font-family: MicrosoftYaHei;
-		font-weight: 400;
-		color: #FC4646;
-		
-	}
-	.ticketCountClass{
-		display: block;
-		margin-top:10upx ;
-		font-size: 24upx;
-		font-family: MicrosoftYaHei;
-		font-weight: 400;
-		color: #999999;
+	
+	.ctky_View_Right {
+		display: flex;
+		text-align: right;
+		align-items: center;
+		padding: 0;
 	}
 </style>
