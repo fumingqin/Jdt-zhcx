12efda3f8e35c1872bdba0ff4c1840e09f5615ba
ChenKz <352646086@qq.com>
2020-03-12 18:51:55 +0800

    时间轴  修改

--- pages/CTKY/selectTickets.vue
+++ pages/CTKY/selectTickets.vue
@@ -3,7 +3,7 @@
 		<view class="headerClass">
 			<scroll-view class="scrollClass" scroll-x>
 				<view class="blockClass" :class="selectIndex == index ? 'viewPress': '' " v-for="(item,index) in dateArray" :key="index"
-				 @click="viewClick(index)">
+				 @click="viewClick(index,item)" v-model="this.selectIndex">
 					<view class="textCLass">
 						<view class="weekClass">{{item.week}}</view>
 						<view class="dateClass">{{item.date}}</view>
@@ -15,7 +15,6 @@
 			</view>
 			<mx-date-picker :show="showPicker" :type="type" :value="value" :show-tips="true" :begin-text="'入住'" :end-text="'离店'"
 			 :show-seconds="true" @confirm="onSelected" @cancel="onSelected" />
-			 
 		</view>
 		<view class="ctky_View" v-for="(item,index) in 2" :key="index" @click="ticketDetail">
 			<view class="ctky_View_Left">
@@ -89,37 +88,37 @@
 		data() {
 			return {
 				dateArray: [
-					{
-						week: '今日',
-						date: '2/28'
-					},
-					{
-						week: '明日',
-						date: '2/29'
-					},
-					{
-						week: '后天',
-						date: '3/1'
-					},
-					{
-						week: '今日',
-						date: '3/2'
-					},
-					{
-						week: '今日',
-						date: '3/3'
-					},
-					{
-						week: '今日',
-						date: '3/4'
-					},
-					{
-						week: '今日',
-						date: '3/5'
-					}
+					// 	{
+					// 		week: '今日',
+					// 		date: '2/28'
+					// 	},
+					// 	{
+					// 		week: '明日',
+					// 		date: '2/29'
+					// 	},
+					// 	{
+					// 		week: '后天',
+					// 		date: '3/1'
+					// 	},
+					// 	{
+					// 		week: '今日',
+					// 		date: '3/2'
+					// 	},
+					// 	{
+					// 		week: '今日',
+					// 		date: '3/3'
+					// 	},
+					// 	{
+					// 		week: '今日',
+					// 		date: '3/4'
+					// 	},
+					// 	{
+					// 		week: '今日',
+					// 		date: '3/5'
+					// 	}
 				],
-				selectIndex: 0,
-				date: '2019/01/01',
+				selectIndex: '',
+				date: '',
 				showPicker: false,
 				type: 'rangetime',
 				value: '',
@@ -137,13 +136,17 @@
 			}
 		},
 		onLoad() {
+			this.date = this.getTime(0, new Date());
+			this.loadDate();
 		},
 		onReady() {
 
 		},
 		methods: {
-			viewClick: function(e) {
+			viewClick: function(e, item) {
 				this.selectIndex = e;
+				this.date = item.longDate;
+				console.log(this.date);
 			},
 			onShowDatePicker(type) { //显示
 				this.type = type;
@@ -164,7 +167,34 @@
 					console.log('value => ' + e.value);
 					//原始的Date对象
 					console.log('date => ' + e.date);
-
+					this.date = e.value;
+					var IsExist = false;
+					for (var i = 0; i < this.dateArray.length; i++) {
+						if ((new Date(this.dateArray[i].longDate)).getTime() == (new Date(this.date)).getTime()) {
+							IsExist = true;
+							this.selectIndex = i;
+						}
+					}
+					if (!IsExist) {
+						this.dateArray = [];
+						var dateToday=new Date();//获取今天日期
+						var date = new Date(this.date);//选中的日期
+						this.selectIndex = 0;
+						for (var i = 0; i < 10; i++) {
+							var mydate = new Date(date.getTime() + 24 * i * 60 * 60 * 1000);
+							var nowdate = this.getTime(3, mydate);
+							var week = this.getTime(2, mydate);
+							var longdate = this.getTime(0, mydate);
+							if (mydate.getTime() == dateToday.getTime()) {
+								week = '今天';
+							}
+							this.dateArray.push({
+								week: week,
+								date: nowdate,
+								longDate: longdate,
+							});
+						}
+					}
 				}
 			},
 			ticketDetail() {
@@ -182,6 +212,92 @@
 					url: "scheduleDetails"
 				})
 			},
+			//type 0 年月日 ，1 时分秒 ， 2 星期 ，3 月/日
+			getTime: function(type, date1) {
+				let date = new Date(date1.getTime()),
+					currentDate,
+					currentTime,
+					sortDate,
+					seperator = "/", // 如果想要其他格式 只需 修改这里 
+					year = date.getFullYear(),
+					month = date.getMonth() + 1,
+					weex = date.getDay(),
+					day = date.getDate(),
+					hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours(),
+					minute = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes(),
+					second = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+				// month >= 1 && month <= 9 ? (month = "0" + month) : "";
+				// day >= 0 && day <= 9 ? (day = "0" + day) : "";
+				//当前 日期
+				currentDate = year + seperator + month + seperator + day;
+				//当前 时间
+				currentTime = hour + ":" + minute + ":" + second;
+				sortDate = month + "/" + day;
+				// 输出格式 为 2018-8-27 14:45:33
+				// console.log(currentDate +" "+ currentTime);
+				if (type == 0) {
+					return currentDate
+				} else if (type == 1) {
+					return currentTime;
+				} else if (type == 2) {
+					if (weex == 1) {
+						return '周一'
+					}
+					if (weex == 2) {
+						return '周二'
+					}
+					if (weex == 3) {
+						return '周三'
+					}
+					if (weex == 4) {
+						return '周四'
+					}
+					if (weex == 5) {
+						return '周五'
+					}
+					if (weex == 6) {
+						return '周六'
+					}
+					if (weex == 0) {
+						return '周日'
+					}
+				} else if (type == 3) {
+					return sortDate;
+				} else {
+					return currentDate + " " + currentTime;
+				}
+			},
+			loadDate() {
+				var date = new Date();
+				console.log(date);
+				this.selectIndex = 0;
+				for (var i = 0; i < 10; i++) {
+					var mydate = new Date(date.getTime() + 24 * i * 60 * 60 * 1000);
+					var nowdate = this.getTime(3, mydate);
+					var week = this.getTime(2, mydate);
+					var longdate = this.getTime(0, mydate);
+					// if(i==3)
+					// {
+					// 	console.log(mydate + week);
+					// 	}
+					if (mydate.getTime() == date.getTime()) {
+						week = '今天';
+					}
+					// if (i == 1) {
+					// 	week = '明天';
+					// }
+					// if (i == 2) {
+					// 	week = '后天';
+					// }
+					//console.log(nowdate + week);
+					this.dateArray.push({
+						week: week,
+						date: nowdate,
+						longDate: longdate,
+					});
+				}
+			}
+
 		}
 	}
 </script>
