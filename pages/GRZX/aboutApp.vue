69104ae11ac7f9b4de5c17755172b85612e61764
Gongjs <1845553629@qq.com>
2020-04-05 14:27:39 +0800

    关于app

--- pages/GRZX/aboutApp.vue
+++ pages/GRZX/aboutApp.vue
@@ -4,16 +4,19 @@
 		<text class="titleClass">{{title}}</text>
 		<text class="versionClass">{{versionNum}}</text>
 		<view class="boxClass1">
-			<view class="functionClass">
+			<view class="functionClass" @click="functionClick">
 				<text class="fontClass">{{functionIntroduction}}</text>
+				<image src="../../static/GRZX/btnRight.png" class="imgClass1"></image>
 			</view>
-			<view class="checkClass">
+			<view class="checkClass" @click="checkClick">
 				<text class="fontClass">{{checkVersion}}</text>
+				<text class="textCLass">{{version}}</text>
+				<image src="../../static/GRZX/btnRight.png" class="imgClass2"></image>
 			</view>
 		</view>
 		<view class="boxClass2">
-			<text class="agreementClass">{{agreement}}</text>
-			<text class="privacyClass">{{privacy}}</text>
+			<text class="agreementClass" @click="agreementClick">{{agreement}}</text>
+			<text class="privacyClass" @click="privacyClick">{{privacy}}</text>
 			<text class="copyrightClass">{{copyright1}}</text>
 			<text class="copyrightClass">{{copyright2}}</text>
 			<text class="copyrightClass">{{copyright3}}</text>
@@ -34,10 +37,37 @@
 				copyright1:'**公司版权所有',
 				copyright2:'Copyright©2019-2020',
 				copyright3:'All Rights Reserved',
+				version:'',
 			}
 		},
+		onLoad(){
+			this.version=plus.runtime.version;
+		},
 		methods:{
-			
+			functionClick(){
+				uni.showToast({
+					icon:'none',
+					title:'功能介绍'
+				})
+			},
+			checkClick(){
+				uni.showToast({
+					icon:'none',
+					title:'检查新版本'
+				})
+			},
+			agreementClick(){
+				uni.showToast({
+					icon:'none',
+					title:'软件许可及服务协议'
+				})
+			},
+			privacyClick(){
+				uni.showToast({
+					icon:'none',
+					title:'隐私保护指引'
+				})
+			},
 		}
 	}
 </script>
@@ -105,15 +135,43 @@
 		margin-left: 3%;
 	}
 	.agreementClass{
-		color: #01B5FD;
+		color: #96a0b4;
 		font-size: 30upx;
+		margin-left: 28%;
+		width: 335upx;
+		height: 30upx;
+		line-height: 30upx;
 	}
 	.privacyClass{
-		color: #0000ff;
+		color: #96a0b4;
 		font-size: 30upx;
+		margin-left: 28%;
+		width: 335upx;
+		height: 30upx;
+		line-height: 30upx;
 	} 
 	.copyrightClass{
-		color: #EAEAEA;
+		color: #bababa;
 		font-size: 26upx;
+	}
+	.imgClass1{
+		width: 45upx;
+		height: 45upx;
+		position: absolute;
+		top:35upx;
+		right: 0%;
+	}
+	.imgClass2{
+		width: 45upx;
+		height: 45upx;
+		position: absolute;
+		top:146upx;
+		right: 0%;
+	}
+	.textCLass{
+		font-size: 30upx;
+		position: absolute;
+		top:116upx;
+		right: 6%;
 	}
 </style>
