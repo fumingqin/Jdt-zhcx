69334c372109f4b82460e289426484a4dc446f47
Crane <crane@CranedeMacBook-Air.local>
2020-04-07 09:25:06 +0800

    修改订单支付，下单页面，传统地图标识

--- pages/CTKY/selectTickets.vue
+++ pages/CTKY/selectTickets.vue
@@ -100,12 +100,11 @@
 		methods: {
 			//加载班次列表数据
 			getTicketInfo:function(date){
-				//加载数据提示菊花
 				uni.showLoading();
 				uni.request({
 					url: "http://27.148.155.9:9055/CTKY/getListSchedulesInfo",
 					data: {
-						systemName:'',
+						systemName:'泉运实业综合出行',
 						startPosition:this.startStation,
 						endPosition:this.endStation,
 						date:date,
