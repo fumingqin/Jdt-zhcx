d309be46d4890939ed5667a9dce5953201e02d85
Shiyj <844349552@qq.com>
2020-04-22 15:00:29 +0800

    mapIndex页面修改

--- pages/Home/MapIndex.vue
+++ pages/Home/MapIndex.vue
@@ -60,21 +60,10 @@
 					longitude: 113.76927057974245,
 					latitude: 34.76670519464811,
 					showLocation: true,
-					iconPath: '../static/Home/icon_position.png',
 					width: 40,
 					height: 40,
 					scale: 16,
-					controls: [{
-						id: 'map',
-						iconPath: '../static/Home/icon_position.png',
-						position: {
-							left: 1200,
-							top: 1200,
-							width: 40,
-							height: 40
-						},
-						clickable: false
-					}]
+					controls: []
 				},
 				list: [],
 				oftenList: [],
