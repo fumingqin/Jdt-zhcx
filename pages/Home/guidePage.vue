b3036b62f6ad294b917caad203c1b962f2e875d9
傅洺钦 <32589407@qq.com>
2020-05-03 20:56:07 +0800

    1

--- pages/Home/guidePage.vue
+++ pages/Home/guidePage.vue
@@ -3,7 +3,7 @@
 		<swiper class="swiper" :style="{'height':imageHeight}">
 			<swiper-item class="swiperItem" v-for="(item,index) in imageIndex" :key="index">
 				<text style="position: absolute; top: 116upx; right: 40upx; color: #ddd; font-size: 30upx;z-index: 99;" @click="natTo">跳过</text>
-				<image class="image" :src="item.src" mode="aspectFill"></image>
+				<image class="image" :src="item.imageUrl" mode="aspectFill"></image>
 				<view style="position: absolute; bottom: 200upx; left: 192upx;" :hidden="index !== 2">
 					<image style="width: 367upx; height: 96upx;" src="../../static/Home/guidePage/y4.png" mode="aspectFill" @click="natTo"></image>
 				</view>
@@ -23,14 +23,15 @@
 		},
 		onLoad: function() {
 			uni.request({
-				url:$lyfw.Interface.spt_GetticketSearchByrequestArea_Six.value,
-				method:$lyfw.Interface.spt_GetticketSearchByrequestArea_Six.method,
+				url:$lyfw.Interface.qg_GetImage.value,
+				method:$lyfw.Interface.qg_GetImage.method,
 				data:{ 
-					// requestArea : this.regionWeixin,
-					requestArea : '南平市'
+					model : 6,
+					type:'引导页'
 				},
 				success:(res) => { 
-					console.log(res)
+					// console.log(res)
+					this.imageIndex = res.data.data
 					
 				},
 			})
