69190cdd170c5dceb7e8cf5551f7b5236bfc7a2d
傅洺钦 <32589407@qq.com>
2020-05-14 15:28:43 +0800

    1

--- pages_LYFW/pages/LYFW/tourismProducts/tp_orderAdd.vue
+++ pages_LYFW/pages/LYFW/tourismProducts/tp_orderAdd.vue
@@ -325,7 +325,7 @@
 			//数组提取
 			screenUser: function() {
 				let adult = this.addressData.filter(item => {
-					return item.userType == '成人';
+					return item.userType == '成人' || item.userType == '军人' || item.userType == '教师' || item.userType == '学生' ;
 				})
 				let children = this.addressData.filter(item => {
 					return item.userType == '儿童';
