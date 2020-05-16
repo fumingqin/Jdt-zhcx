1ade68da8af798068543f43593ef9fbfbbcb30fd
傅洺钦 <32589407@qq.com>
2020-05-16 11:06:25 +0800

    1

--- pages/order/OrderList.vue
+++ pages/order/OrderList.vue
@@ -3696,7 +3696,7 @@
 						'content-type': 'application/json'
 					},
 					success: (res) => {
-						// console.log(res);
+						console.log('旅游产品',res);
 						if (res.data.status == true) {
 							for (var i = 0; i < res.data.data.length; i++) {
 									that.info.push(res.data.data[i]);
