e0e12da38008ad98f35a84ac901ef27f12a37a3c
Crane <crane@CranedeMacBook-Air.local>
2020-05-29 11:50:15 +0800

    修改

---
+++ pages_DDQC/pages/RentBike/RentBikeHome.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<RentBike>
+			
+		</RentBike>
+	</view>
+</template>
+
+<script>
+	import RentBike from './RentBike.nvue'
+	components:{
+		RentBike
+	}
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
