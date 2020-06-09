218020afe92f71364b74b29bf4e7800545512414
caiweida <2332268008@qq.com>
2020-06-09 11:10:59 +0800

    111111111

---
+++ pages_DDQC/pages/GRZY/zy_newsScreening.vue
@@ -0,0 +1,43 @@
+<template>
+	<view>
+		<view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	import $lyfw from '@/common/LYFW/LyfwFmq.js' //引用路径
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		
+		onLoad() {
+			this.loadData();
+		},
+		
+		methods: {
+			loadData: function() {
+				//请求新闻资讯
+				uni.request({
+					url:$lyfw.Interface.currency_zhly.value,
+					method:$lyfw.Interface.currency_zhly.method,
+					success: (e) => {
+						console.log(e)
+						this.goodsList = e.data.data;
+						
+					}
+				})
+				
+				uni.stopPullDownRefresh();
+			},
+		}
+	}
+</script>
+
+<style>
+
+</style>
