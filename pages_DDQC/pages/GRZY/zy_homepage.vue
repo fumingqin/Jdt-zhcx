6a09c70d1cd85c4321a3b715c7fb6dba7e3a9e60
caiweida <2332268008@qq.com>
2020-06-18 09:10:18 +0800

    1111

--- pages_DDQC/pages/GRZY/zy_homepage.vue
+++ pages_DDQC/pages/GRZY/zy_homepage.vue
@@ -623,37 +623,43 @@
 					},
 					success(res) {
 						console.log(res)
-						if (res.data.data == "" || res.data.data.UserName == "" || res.data.data.UserIDNumber == "") {
-							//实名认证
-							uni.navigateTo({
-								url: that.$GrzxInter.Route.realName.url,
-							})
-						} else if (res.data.data.RealNameStatus !== 1) {
-							//上传图片
-							uni.navigateTo({
-								url: that.$GrzxInter.Route.uploadPhoto.url,
-							})
-						} else {
-							if (e == 1) {
+						if(that.userInfo!==''){
+							if (res.data.data == "" || res.data.data.UserName == "" || res.data.data.UserIDNumber == "") {
+								//实名认证
 								uni.navigateTo({
-									url: './wallet'
+									url: that.$GrzxInter.Route.realName.url,
 								})
-							} else if (e == 2) {
+							} else if (res.data.data.RealNameStatus !== 1) {
+								//上传图片
 								uni.navigateTo({
-									url: './topUp'
+									url: that.$GrzxInter.Route.uploadPhoto.url,
 								})
-							} else if (e == 3) {
-								// 需要在 popup 组件，指定 ref 为 popup
-								that.$refs.popup.open()
-							} else if (e == 4) {
-								// 需要在 popup 组件，指定 ref 为 popup
-								that.$refs.popup2.open()
-							} else if (DepositType == 2) {
-								uni.showToast({
-									title: '免押金用户',
-									icon: 'none'
-								})
+							} else {
+								if (e == 1) {
+									uni.navigateTo({
+										url: './wallet'
+									})
+								} else if (e == 2) {
+									uni.navigateTo({
+										url: './topUp'
+									})
+								} else if (e == 3) {
+									// 需要在 popup 组件，指定 ref 为 popup
+									that.$refs.popup.open()
+								} else if (e == 4) {
+									// 需要在 popup 组件，指定 ref 为 popup
+									that.$refs.popup2.open()
+								} else if (DepositType == 2) {
+									uni.showToast({
+										title: '免押金用户',
+										icon: 'none'
+									})
+								}
 							}
+						}else{
+							uni.navigateTo({
+								url:'../../../pages/GRZX/userLogin'
+							})
 						}
 					}
 				})
